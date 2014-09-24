a11b76e0ca6da968e1a924240557b925af8da671
evanmbrown <evanmbrown84@gmail.com>
2014-09-23 23:40:03 -0400

    Telling everyone about my fav ice cream flavor in way too much detail

---
+++ icecream.rb
@@ -0,0 +1,3 @@
+# My favorite flavor of ice cream of all time is Ben & Jerry's old Coffee Heath Bar Crunch. 
+# They discontinued it and now have a Coffee Toffee Bar Crunch.
+# Which is like getting Hunts ketchup when you want the real thing (Heinz obviously).
